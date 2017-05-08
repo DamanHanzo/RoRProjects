d7cdb1b3ea5eca3f2334ca00451e97855cb52885
damanhanzo <singhdamanpreet1234@gmail.com>
2017-05-07 20:54:25 -0400

    adding remaining CRUD functionality

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -4,11 +4,16 @@ class ArticlesController < ApplicationController
 		@articles = Article.all
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	def show
 		@article = Article.find(params[:id])
 	end
 
 	def new
+		@article = Article.new
 	end
 
 	def create
@@ -20,6 +25,20 @@ class ArticlesController < ApplicationController
 			render 'new'
 		end
 	end
+	def update 
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
 	private
 		def article_params
 			params.require(:article).permit(:title, :text)
